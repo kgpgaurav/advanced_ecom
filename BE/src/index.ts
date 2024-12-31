dc789639477d88b802cfd3fa4ee27dafa54f811a
kgpgaurav <kgpgaurave@gmail.com>
2024-12-31 18:28:55 +0530

    first commit for this project

---
+++ BE/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app= express();
+
+app.use(express.json());
+
+app.post("/api/v1/signup", (req,res)=>{
+
+})
+
+app.post("/api/v1/signin", (req,res)=>{
+    
+})
+
+app.post("/api/v1/signup", (req,res)=>{
+    
+})
+
+app.listen(3000);
